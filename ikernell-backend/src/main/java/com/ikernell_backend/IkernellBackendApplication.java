3899f471a76671260848278558c5496122fa5d6f
Milord-Taro <efraina.manotasc@gmail.com>
2026-06-12 16:54:43 -0500

    Proyecto inicial Ikernell

---
+++ ikernell-backend/src/main/java/com/ikernell_backend/IkernellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ikernell_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkernellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkernellBackendApplication.class, args);
+	}
+
+}
---
+++ ikernell-backend/src/test/java/com/ikernell_backend/IkernellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikernell_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkernellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
