592c29eb79bbc5dc3b504bcfb2ff6e8f80a92823
Milord-Taro <efraina.manotasc@gmail.com>
2026-06-12 16:54:43 -0500

    Proyecto inicial Ikernell

---
+++ ikernell-backend/src/main/java/com/ikernell_backend/entity/Profesion.java
@@ -0,0 +1,45 @@
+package com.ikernell_backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "profesion")
+public class Profesion {
+
+    @Id
+    @Column(name = "idprofesion")
+    private Integer idProfesion;
+
+    @Column(name = "codprofesion")
+    private String codProfesion;
+
+    @Column(name = "nombreprofesion")
+    private String nombreProfesion;
+
+    public Profesion() {
+    }
+
+    public Integer getIdProfesion() {
+        return idProfesion;
+    }
+
+    public void setIdProfesion(Integer idProfesion) {
+        this.idProfesion = idProfesion;
+    }
+
+    public String getCodProfesion() {
+        return codProfesion;
+    }
+
+    public void setCodProfesion(String codProfesion) {
+        this.codProfesion = codProfesion;
+    }
+
+    public String getNombreProfesion() {
+        return nombreProfesion;
+    }
+
+    public void setNombreProfesion(String nombreProfesion) {
+        this.nombreProfesion = nombreProfesion;
+    }
+}
